26fdf619783dbd00ead76722f4d87c090c3267c1
zackarymeadows <110922251+zackarymeadows@users.noreply.github.com>
2022-08-29 20:38:57 -0400

    initial python upload

---
+++ Election Analysis/practice_python.py
@@ -0,0 +1,3 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters")
